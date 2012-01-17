bced3bf70b7adadfbabb62510df99d2fc5a897c1
Andrey Popp <8mayday@gmail.com>
2012-01-17 15:18:58 +0400

    add array attributes

--- sphinxalchemy/__init__.py
+++ sphinxalchemy/__init__.py
@@ -1,11 +1,11 @@
 """ SphinxAlchemy"""
 
 from sphinxalchemy.sphinxql import select
-from sphinxalchemy.schema import Index, Attribute
+from sphinxalchemy.schema import Index, Attribute, ArrayAttribute
 from sqlalchemy import MetaData, create_engine
 from sqlalchemy.sql import func
 
 __all__ = (
-    "Index", "Attribute", "MetaData",
+    "Index", "Attribute", "ArrayAttribute",
     "select", "func",
-    "create_engine")
+    "create_engine", "MetaData")
--- sphinxalchemy/schema.py
+++ sphinxalchemy/schema.py
@@ -21,5 +21,27 @@ class Attribute(Column):
         kwargs["type_"] = types.Integer()
         super(Attribute, self).__init__(*args, **kwargs)
 
+class ArrayAttribute(Column):
+
+    def __init__(self, *args, **kwargs):
+        args = list(args)
+        if len(args) > 1 and isinstance(args[1], (types.TypeEngine, type)):
+            args.pop(1)
+        kwargs["type_"] = ArrayAttributeType()
+        super(ArrayAttribute, self).__init__(*args, **kwargs)
+
 class SpecialAttribute(Attribute):
     pass
+
+class ArrayAttributeType(types.TypeDecorator):
+
+    impl = types.String
+
+    def process_bind_param(self, value, dialect):
+        return ",".join(map(str, value))
+
+    def process_result_value(self, value, dialect):
+        return map(int, value.split(","))
+
+    def copy(self):
+        return ArrayAttributeType()
