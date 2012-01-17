cedca58c4fb09cffe263bcd625a9c2361f8bcddb
Andrey Popp <8mayday@gmail.com>
2012-01-17 14:16:42 +0400

    sphinxalchemy.select(): SphinxQL extended SELECT
      * supports WITHIN GROUP ORDER BY
    
      * supports OPTION clause

--- sphinxalchemy/dialect.py
+++ sphinxalchemy/dialect.py
@@ -3,11 +3,135 @@
 from sqlalchemy.connectors import mysqldb
 from sqlalchemy.engine import default
 from sqlalchemy.sql import compiler
+from sqlalchemy.sql import expression as sql
+from sqlalchemy import util
 
 __all__ = ("SphinxDialect",)
 
 class SphinxCompiler(compiler.SQLCompiler):
-    pass
+
+    def within_group_order_by_clause(self, select, **kw):
+        order_by = select._within_group_order_by_clause._compiler_dispatch(
+            self, **kw)
+        if order_by:
+            return " WITHIN GROUP ORDER BY " + order_by
+        else:
+            return ""
+
+    def options_clause(self, select, **_kw):
+        options = ", ".join("%s=%s" % (k, v) for (k, v) in select._options)
+        return " OPTION %s" % options
+
+    def visit_select(self, select, asfrom=False, parens=True,
+                            iswrapper=False, fromhints=None,
+                            compound_index=1, **kwargs):
+
+        entry = self.stack and self.stack[-1] or {}
+
+        existingfroms = entry.get('from', None)
+
+        froms = select._get_display_froms(existingfroms)
+
+        correlate_froms = set(sql._from_objects(*froms))
+
+        # TODO: might want to propagate existing froms for
+        # select(select(select)) where innermost select should correlate
+        # to outermost if existingfroms: correlate_froms =
+        # correlate_froms.union(existingfroms)
+
+        self.stack.append({'from': correlate_froms, 'iswrapper'
+                          : iswrapper})
+
+        if compound_index==1 and not entry or entry.get('iswrapper', False):
+            column_clause_args = {'result_map':self.result_map}
+        else:
+            column_clause_args = {}
+
+        # the actual list of columns to print in the SELECT column list.
+        inner_columns = [
+            c for c in [
+                self.label_select_column(select, co, asfrom=asfrom).\
+                    _compiler_dispatch(self,
+                        within_columns_clause=True,
+                        **column_clause_args)
+                for co in util.unique_list(select.inner_columns)
+            ]
+            if c is not None
+        ]
+
+        text = "SELECT "  # we're off to a good start !
+
+        if select._hints:
+            byfrom = dict([
+                            (from_, hinttext % {
+                                'name':from_._compiler_dispatch(
+                                    self, ashint=True)
+                            })
+                            for (from_, dialect), hinttext in
+                            select._hints.iteritems()
+                            if dialect in ('*', self.dialect.name)
+                        ])
+            hint_text = self.get_select_hint_text(byfrom)
+            if hint_text:
+                text += hint_text + " "
+
+        if select._prefixes:
+            text += " ".join(
+                            x._compiler_dispatch(self, **kwargs)
+                            for x in select._prefixes) + " "
+        text += self.get_select_precolumns(select)
+        text += ', '.join(inner_columns)
+
+        if froms:
+            text += " \nFROM "
+
+            if select._hints:
+                text += ', '.join([f._compiler_dispatch(self,
+                                    asfrom=True, fromhints=byfrom,
+                                    **kwargs)
+                                for f in froms])
+            else:
+                text += ', '.join([f._compiler_dispatch(self,
+                                    asfrom=True, **kwargs)
+                                for f in froms])
+        else:
+            text += self.default_from()
+
+        if select._whereclause is not None:
+            t = select._whereclause._compiler_dispatch(self, **kwargs)
+            if t:
+                text += " \nWHERE " + t
+
+        if select._group_by_clause.clauses:
+            group_by = select._group_by_clause._compiler_dispatch(
+                                        self, **kwargs)
+            if group_by:
+                text += " GROUP BY " + group_by
+
+        if select._having is not None:
+            t = select._having._compiler_dispatch(self, **kwargs)
+            if t:
+                text += " \nHAVING " + t
+
+        if select._order_by_clause.clauses:
+            text += self.order_by_clause(select, **kwargs)
+        if getattr(select, "_within_group_order_by_clause", None) is not None:
+            if select._within_group_order_by_clause.clauses:
+                text += self.within_group_order_by_clause(select, **kwargs)
+        if select._limit is not None or select._offset is not None:
+            text += self.limit_clause(select)
+        if getattr(select, "_options", None) is not None:
+            if select._options.options:
+                text += self.options_clause(select, **kwargs)
+        if select.for_update:
+            text += self.for_update_clause(select)
+
+        self.stack.pop(-1)
+
+        if asfrom and parens:
+            return "(" + text + ")"
+        else:
+            return text
 
 class SphinxDialect(default.DefaultDialect, mysqldb.MySQLDBConnector):
 
--- sphinxalchemy/sphinxql.py
+++ sphinxalchemy/sphinxql.py
@@ -4,8 +4,65 @@ from sqlalchemy.sql import expression
 
 __all__ = ("SelectClause", "select")
 
-class SelectClause(expression.SelectClause):
-    pass
+class _Options(object):
 
-def select(*args, **kwargs):
-    return SelectClause(*args, **kwargs)
+    def __init__(self, options):
+        self.options = list(options)
+
+    def extend(self, options):
+        self.options.extend(options)
+
+    def _clone(self):
+        return _Options(self.options[:])
+
+    def __iter__(self):
+        return iter(self.options)
+
+class Select(expression.Select):
+
+    _within_group_order_by_clause = expression.ClauseList()
+    _options = None
+
+    @expression._generative
+    def within_group_order_by(self, *clauses):
+        self.append_within_group_order_by(*clauses)
+
+    @expression._generative
+    def options(self, *args, **kwargs):
+        options = list(args) + kwargs.items()
+        if self._options is None:
+            self._options = _Options(options)
+        else:
+            self._options.extend(options)
+
+    def append_within_group_order_by(self, *clauses):
+        """Append the given WITHIN GROUP ORDER BY criterion applied to this
+        selectable.
+
+        The criterion will be appended to any pre-existing WITHIN GROUP ORDER BY
+        criterion.
+        """
+        if len(clauses) == 1 and clauses[0] is None:
+            self._within_group_order_by_clause = expression.ClauseList()
+        else:
+            if getattr(self, '_within_group_order_by_clause', None) is not None:
+                clauses = list(self._within_group_order_by_clause) \
+                        + list(clauses)
+            self._within_group_order_by_clause = expression.ClauseList(*clauses)
+
+    def _copy_internals(self, clone=expression._clone, **kw):
+        super(Select, self)._copy_internals(clone=clone, **kw)
+        for attr in '_within_group_order_by_clause', '_options':
+            if getattr(self, attr) is not None:
+                setattr(self, attr, clone(getattr(self, attr), **kw))
+
+    def get_children(self, column_collections=True, **kwargs):
+        """return child elements as per the ClauseElement specification."""
+        c = super(Select, self).get_children(
+            column_collections=column_collections, **kwargs)
+        return c + [x for x
+                    in self._within_group_order_by_clause
+                    if x is not None]
+
+def select(columns=None, whereclause=None, from_obj=[], **kwargs):
+    return Select(columns, whereclause=whereclause, from_obj=from_obj, **kwargs)
