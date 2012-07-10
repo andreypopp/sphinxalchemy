f1a3834e1f00b7f8d917ee2c1286602cd2093070
Yegor Nazarkin <nimnull@gmail.com>
2012-07-10 23:11:42 +0300

    Fixed setup. Renamed README

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="sphinxalchemy",
     version=version,
     description="SQLAlchemy extension for dealing with SphinxQL",
-    long_description=open("README", "r").read(),
+    long_description=open("README.rst", "r").read(),
     author="Andrey Popp",
     author_email="8mayday@gmail.com",
     packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
